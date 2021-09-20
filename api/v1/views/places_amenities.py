3d609ab36bb6402bc42b3d206a4bcd14710c20d0
aike-s <sagrilo804@gmail.com>
2021-09-19 21:01:50 -0500

    [add] new RESTful API service for place amenities

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+All default RESTFul API actions for review objects
+"""
+from api.v1.views import app_views
+from flask import request, abort
+from flask.json import jsonify
+from models import storage
+from models.place import Place
+from models.amenity import Amenity
+
+
+@app_views.route('/api/v1/places/<int:place_id>/amenities', method=['GET'],
+                 strict_slashes=False)
+def get_all_amenities(place_id):
+    """ Retrieves a list of all amenity objects based on the place id"""
+    all_place_amenities = []
+    amenity_objs = storage.all("Amenity")
+
+    for obj in amenity_objs.values():
+        obj = obj.to_dict()
+        if obj['place_id'] is place_id:
+            all_place_amenities.append(obj)
+
+    if len[all_place_amenities] < 1:
+        abort(404)
+    else:
+        return jsonify(all_place_amenities)
+
+
+@app_views.route('/api/v1/places/<int:place_id>/amenities/<int:amenity_id>',
+                 method=['DELETE'], strict_slashes=False)
+def delete_review(place_id, amenity_id):
+    """ Deletes a review object based on the review id """
+    amenity_obj = storage.get(Amenity, amenity_id)
+    place_obj = storage.get(Place, place_id)
+
+    if amenity_obj is None:
+        abort(404)
+    if place_obj is None:
+        abort(404)
+    if amenity_obj.to_dict()["place_id"] is not place_id:
+        abort(404)
+    else:
+        storage.delete(amenity_obj)
+        return jsonify({}), 200
+
+
+@app_views.route('/api/v1/places/<place_id>/amenities/<amenity_id>',
+                 methods=['POST'], strict_slashes=False)
+def post_amenity(place_id, amenity_id):
+    """ Creates a new amenity """
+    place_obj = storage.get(Place, place_id)
+    amenity_obj = storage.get(Amenity, amenity_id)
+
+    if place_obj is None:
+        abort(404)
+    if amenity_obj is None:
+        abort(404)
+
+    amenity_dict = amenity_obj.to_dict()
+
+    if amenity_dict["place_id"] is place_id:
+        return jsonify(amenity_dict), 200
+    else:
+        amenity_dict["place_id"] = place_id
+        return jsonify(amenity_dict), 201
