e7165b91a70a5c4498cf64ac3886d72adeaf707f
aike-s <sagrilo804@gmail.com>
2021-09-20 18:33:19 -0500

    [fix] pep8 style

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,6 +12,7 @@ from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ Return the status """
